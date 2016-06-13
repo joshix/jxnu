8e4c12b43d988e15bcbf677a8e19ab1d0f92e1dc
Joshua Wood <j@joshix.com>
2016-06-12 20:50:41 -0700

    acbuild - sq

--- acbuild-caddy.bash
+++ acbuild-caddy.bash
@@ -18,24 +18,8 @@ acbuild --debug set-name joshix.com/jxnu
 acbuild --debug copy-to-dir ./public /var/www
 acbuild --debug copy ./Caddyfile /Caddyfile
 
-# Add ports for HTTP, HTTPS, and Caddy's default unprivileged port.
-#acbuild --debug port add http tcp 80
-#acbuild --debug port add https tcp 443
-#acbuild --debug port add http-alt tcp 2015
-
-# Add expected mount point for the let's encrypt key store. Don't enumerate
-# the html directory or the Caddyfile, so that they won't have an empty
-# directory mounted over them.
-# This allows for the simple case: 'rkt run --port http-alt:2015 caddy.aci'
-# will serve the included default index.html over port 2015.
-# To serve a real site, provide volumes for all three of these to rkt run,
-# along with mounts for the first two. Add --dns options for name resolution.
-#acbuild --debug mount add caddyfile /Caddyfile
-#acbuild --debug mount add html /var/www/html
-#acbuild --debug mount add dotcaddy /root/.caddy
-
-# How to execute caddy
-#acbuild --debug set-exec -- /bin/caddy -root /var/www/public
+# How to execute caddy - explicitly change the served directory to public
+acbuild --debug set-exec -- /bin/caddy -root /var/www/public
 
 # Save the ACI
 acbuild --debug write --overwrite jxnu-v0.9-caddy-v0.8.3-linux-amd64.aci
