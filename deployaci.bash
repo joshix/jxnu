4390ffc35433b01578bac263000ab0676cbc9e4c
Joshua Wood <j@joshix.com>
2016-06-17 19:31:23 -0700

    deployaci.bash: Parameterize the aci builder

--- deployaci.bash
+++ deployaci.bash
@@ -3,16 +3,31 @@
 # Convenience script. Run on the mac to get a new ACI from an acbuild VM
 # and upload it to the rkt host.
 
+# A BLDR is set up like (usually beneath $HOME):
+#  jxnu-aci/
+#			bin/
+#					acbuild
+#					hugo
+#			caddybox/ (from git clone -b acbuild)
+#			jxnu/	  (from git clone -b master)
+#				themes/bootstrap/ (from git clone of sub-repo, for now)
+#
+# $ ssh $BLDR
+# $ export PATH=$PATH:$HOME/jxnu-aci/bin
+# [ $ cd jxnu-aci/caddybox && ./acbuild-caddybox.bash ] (Usually don't rebuild caddybox aci.)
+# $ cd jxnu-aci/jxnu && ./mknu.bash
+
 ACI=jxnu-v1.0-caddy-v0.8.3-linux-amd64.aci
-SSH=core@joshix.com
+BLDR=core@jxnu.joshix.com
+#BLDR=j@chapeau.local
+#BLDR=-i ../acbuild/.vagrant/machines/default/virtualbox/private_key -P 2202 vagrant@127.0.0.1
+SRVR=core@joshix.com
 
-#scp -i ../acbuild/.vagrant/machines/default/virtualbox/private_key -P 2202 \
-#vagrant@127.0.0.1:jxnu/$ACI .
-scp j@chapeau.local:jxnu/$ACI .
+scp $BLDR:jxnu-aci/jxnu/$ACI .
 
-scp $ACI $SSH:
+scp $ACI $SRVR:
 
-ssh $SSH sudo systemctl restart jxnu-aci
-#ssh $SSH sudo rkt gc --grace-period=0
-#ssh $SSH sudo rkt image gc --grace-period=0
-#ssh $SSH sudo systemctl start jxnu-aci
+ssh $SRVR sudo systemctl restart jxnu-aci
+#ssh $SRVR sudo rkt gc --grace-period=0
+#ssh $SRVR sudo rkt image gc --grace-period=0
+#ssh $SRVR sudo systemctl start jxnu-aci
