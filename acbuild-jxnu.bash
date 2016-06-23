64f3fd296d5ea6c416e02b7e738f2bbba42322db
Josh Wood <j@coreos.com>
2016-06-22 22:37:18 -0700

    acbuild-jxnu.bash: Use the acbuild and hugo expected in ../bin
    Now we do not have to set the PATH on a BLDR.

--- acbuild-jxnu.bash
+++ acbuild-jxnu.bash
@@ -1,34 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
+ACBUILD=../bin/acbuild
+HUGO=../bin/hugo
+
 # Builds the site, builds an ACI.
 # Requires hugo and acbuild.
 
 # Generate the html
 rm -rf public
-hugo
+$HUGO
 
 # Start the build with the caddybox ACI. Not necessary
 # to rebuild it with every site content update.
 # Assumes the base ACI is in ../caddybox/.
-acbuild --debug begin ../caddybox/caddy-v0.8.3-linux-amd64.aci
+$ACBUILD --debug begin ../caddybox/caddy-v0.8.3-linux-amd64.aci
 
 # In the event of the script exiting, end the build
 acbuildEnd() {
     export EXIT=$?
-    acbuild --debug end && exit $EXIT
+    $ACBUILD --debug end && exit $EXIT
 }
 trap acbuildEnd EXIT
 
 # Name the ACI
-acbuild --debug set-name joshix.com/jxnu
+$ACBUILD --debug set-name joshix.com/jxnu
 
 # ./public must exist. See mknu.bash to sync and generate it with hugo.
-acbuild --debug copy-to-dir ./public /var/www
-acbuild --debug copy ./Caddyfile /Caddyfile
+$ACBUILD --debug copy-to-dir ./public /var/www
+$ACBUILD --debug copy ./Caddyfile /Caddyfile
 
 # How to execute caddy - explicitly change the served directory to public
-acbuild --debug set-exec -- /bin/caddy -root /var/www/public
+$ACBUILD --debug set-exec -- /bin/caddy -root /var/www/public
 
 # Save the ACI
-acbuild --debug write --overwrite jxnu-v1.0-caddy-v0.8.3-linux-amd64.aci
+$ACBUILD --debug write --overwrite jxnu-v1.0-caddy-v0.8.3-linux-amd64.aci
